b3bf43f148664a6e7cfb87a68fa7abfbddb91ed0
Mikhail Shogin <mshogin@gmail.com>
2021-01-09 20:28:25 +0100

    Add tensorflow image

--- dpkg-buildpackage/build.sh
+++ dpkg-buildpackage/build.sh
@@ -2,4 +2,4 @@
 
 set -x
 docker build --build-arg UBUNTU_VERSION=18.04 . -t mshogin/dpkg-buildpackage:bionic
-docker build --build-arg UBUNTU_VERSION=20.04 . -t mshogin/dpkg-buildpackage:falcon
+docker build --build-arg UBUNTU_VERSION=20.04 . -t mshogin/dpkg-buildpackage:focal
