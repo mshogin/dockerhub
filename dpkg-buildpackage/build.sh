5d0e2bd16f6357a897549ef38b954d8aeeb8796f
Mikhail Shogin <mshogin@gmail.com>
2021-06-04 18:01:39 +0200

    terraform with gcloud

--- dpkg-buildpackage/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -x
-docker build --build-arg UBUNTU_VERSION=18.04 . -t mshogin/dpkg-buildpackage:bionic
-docker build --build-arg UBUNTU_VERSION=20.04 . -t mshogin/dpkg-buildpackage:focal
